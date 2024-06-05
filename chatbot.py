5a1273a9c387543342be0f62cde4ae346dddc3a5
SreekarSre <pulukurthisreekar1@gmail.com>
2024-06-05 12:59:38 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,21 @@
+print('AI:"what is your name?"')
+name = input("you: ")
+print(" ")
+
+print('AI:"where are you from?"')
+country = input("you: ")
+print(" ")
+
+print('AI:"what are your hobbies?"')
+hobbies =input("you: ")
+print(" ")
+
+import time 
+print("...")
+time.sleep(1)
+print("....")
+time.sleep(1)
+print(".....")
+time.sleep(1)
+
+print('AI: Hello! ' + name + 'from' + country + 'that enjoys' + hobbies )
